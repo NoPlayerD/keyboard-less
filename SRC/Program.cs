55b09cc6fd46dd570dba19de085d559629bf0c88
NoPlayerD <jadonredninja@gmail.com>
2024-05-28 20:57:13 +0300

    init commit, project created. void check created, roots.cs created..

---
+++ SRC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Spectre.Console;
+
+
+string _path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+string __path = _path + @"/.KEYBOARDLESS";
+
+check();
+
+Folder root = new Folder();
+
+foreach (string f in Directory.GetDirectories(__path))
+{
+    root.name = f.Remove(0,f.LastIndexOf(@"\")+1);
+    root.path = f;
+    Console.WriteLine(root.name);
+}
+
+
+
+void check()
+{
+    if (!Directory.Exists(__path))
+    {
+        Directory.CreateDirectory(__path);
+    }
+}
---
+++ SRC/roots.cs
@@ -0,0 +1,8 @@
+using System.Runtime.Intrinsics.X86;
+
+public class Folder
+{
+public string name { get; set; }
+public string path { get; set; }
+
+}
